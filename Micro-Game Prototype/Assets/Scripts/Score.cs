2ec92b8d693ea45a5a0b7f80009e5362b3862061
adambrewer <55362205+adambrewer@users.noreply.github.com>
2021-03-28 03:39:27 -0400

    Adam's Optimized Version Attempt

--- Micro-Game Prototype/Assets/Scripts/Score.cs
+++ Micro-Game Prototype/Assets/Scripts/Score.cs
@@ -6,20 +6,12 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public float scoreAmount;
-    public float pointIncrease;
     public Text scoreText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        scoreAmount = 0f;
-        pointIncrease = 30f;
-    }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = (int)scoreAmount + "";
-        scoreAmount += pointIncrease * Time.deltaTime;
+        scoreAmount += 30f * Time.deltaTime;
     }
 }
